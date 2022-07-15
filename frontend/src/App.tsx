c25e01aa890553b9644113e25dd4b9afe0be9345
diogo <diogo12h91@gmail.com>
2022-07-15 10:02:52 -0300

    Other Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,23 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 
 
 function App() {
-  return(
+  return (
     <>
-      <div>teste</div>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
-  
+
 }
 
 export default App
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import Logoicon from '../../assets/logo.svg';
+import './style.css';
+
+function Header() {
+    return(
+        <header>
+        <div className="dsmeta-logo-container">
+            <img src={Logoicon} alt="DSMeta" />
+            <h1>DSMeta</h1>
+            <p>
+              Desenvolvido por 
+              <a href="https://www.instagram.com/diogo_hsc/"> @Diogo_hsc</a>
+            </p>
+        </div>
+    </header>
+    )
+    
+  }
+  
+  export default Header
